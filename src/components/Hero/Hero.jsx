e35c36f816cb5a3d82315d68c2f859c90558897c
Gocra <116901522+Gocra@users.noreply.github.com>
2023-01-29 22:31:47 +0000

    Add files via upload

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,14 +3,7 @@ import "./Hero.css"
 const Hero = () => {
   return (
     <section className="hero">
-        <img src={heroBGImg} alt="" />
-        <img src={heroImg} alt="" />
-        <div className="hero-deal">
-            <h1>82% off</h1>
-            <p>+2 mo. FREE</p>
-            <p>One VPN deal to rock them all</p>
-            <a href="" className="btn btn-md btn-light">Get VPN Deal</a>
-        </div>
+        
     </section>
   )
 }
